31d15fdc4561162f42d9cdad1581db101e7ea5d7
Win8PC2014 <win8.pc.2014@outlook.com>
2015-07-09 12:38:48 -0400

    Initial commit.

---
+++ src/com/broadway/customclock/BitClockWidgetActivity.java
@@ -0,0 +1,15 @@
+package com.broadway.customclock;
+
+import com.broadway.customclock.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BitClockWidgetActivity extends Activity {
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.widgetlayout);
+    }
+}
